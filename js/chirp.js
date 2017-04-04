8e4d540c0ac5cba24db6ef3a1b6c7c159376af88
Ernest Tsang <kwaimuk@Ernests-MacBook-Air.local>
2017-04-04 10:49:51 -1000

    chirp

--- js/chirp.js
+++ js/chirp.js
@@ -1,7 +1,17 @@
 function chirp(n){
-  // FIX ME
-}
 
+  // FIX ME
+
+  if(n === 1){
+
+    return "chirp";
+  }
+
+  return  "chirp " + chirp(--n);
+
+  }
+
+console.log(chirp(3));
 $(document).ready(function(){
   $("#result").html(chirp(3));
 });
